b58e51d4b5dd670ed44d03bdb3d0ec8f6afea9c3
Grigory Yaremenko <yaremenko8@gmail.com>
2022-12-19 14:07:08 +0300

    Added features for convenient instantiation (~), python inline for config fields (get:) and instance identity control (same:). Also altered parsing behaviors in such a way that escaping parentheses and brackets is no longer necessary.

--- rcognita/__init__.py
+++ rcognita/__init__.py
@@ -1,9 +1,11 @@
 __version__ = "v0.1.2"
 
+
 import omegaconf
 
 from omegaconf import DictConfig, OmegaConf
 from omegaconf.resolvers import oc
+import re
 
 from typing import Any, Optional
 
@@ -11,11 +13,18 @@ from omegaconf import Container, Node
 from omegaconf._utils import _DEFAULT_MARKER_, _get_value
 from omegaconf.basecontainer import BaseContainer
 from omegaconf.errors import ConfigKeyError
-from omegaconf.grammar_parser import parse
+from omegaconf.grammar_parser import *
+from omegaconf.grammar_parser import _grammar_cache
 from omegaconf.resolvers.oc import dict
 
-import hydra
-from hydra.utils import get_class, instantiate
+import antlr4
+from . import __fakeantlr4
+from recursive_monkey_patch import monkey_patch
+
+monkey_patch(__fakeantlr4, antlr4)
+
+
+from hydra.utils import instantiate
 
 
 from . import controllers
@@ -30,8 +39,14 @@ from . import predictors
 from . import actors
 import colored_traceback
 import numpy
+
 from unittest.mock import Mock
 from hydra._internal.utils import _locate
+
+
+
+
+
 
 
 mock = Mock()
@@ -44,32 +59,67 @@ def memorize_instance(resolver):
         key: str, default: Any = _DEFAULT_MARKER_, *, _parent_: Container,
     ) -> Any:
         obj = instantiate(resolver(key, default=default, _parent_=_parent_))
-        if obj.__class__.__name__ in objects_created:
-            return objects_created[obj.__class__.__name__]
+        key = obj.__class__.__name__ + str(default)
+        if key in objects_created:
+            return objects_created[key]
         else:
-            objects_created[obj.__class__.__name__] = obj
+            objects_created[key] = obj
             return obj
 
     return inner
 
 
+def sub_map(pattern, f, s):
+    def map_match(match):
+        return f(match.group())
+    return re.sub(pattern, map_match, s)
+
+
 def obtain(obj_repr):
-    obj_repr = obj_repr.replace(")", ",)")
-    if "(" in obj_repr:
-        i = obj_repr.find("(")
-        func = _locate(obj_repr[:i])
-        return func(*eval(obj_repr[i:]))
-    else:
-        return _locate(obj_repr)
+    pattern = re.compile(r'^\\.[a-zA-Z]+')
+    resolved = []
+    def resolve(s):
+        resolved.append(_locate(s))
+        return f"resolved[{len(resolved) -  1}]"
+    obj_repr_resolved_modules = sub_map(pattern, resolve, obj_repr)
+    return eval(obj_repr_resolved_modules)
 
 
-# OmegaConf.register_new_resolver(name="get_cls", resolver=lambda cls: get_class(cls))
+
+
 OmegaConf.register_new_resolver("same", memorize_instance(oc.select))
 OmegaConf.register_new_resolver(name="get", resolver=obtain)
 OmegaConf.register_new_resolver(name="mock", resolver=lambda: mock)
-# OmegaConf.register_new_resolver(name="rcognita", resolver=lambda var: get_class(f"rcognita.{var}"))
+
 
 colored_traceback.add_hook()
+
+from hydra import main as hydramain
+
+
+class ComplementedConfigWrapper:
+    def __init__(self, cfg):
+        self.cfg = cfg
+
+    def __getattr__(self, item):
+        cfg = object.__getattribute__(self, "cfg")
+        return ComplementedConfigWrapper(cfg.__getattr__(item))
+
+    def __str__(self):
+        return str(self.cfg)
+
+    def __repr__(self):
+        return self.__class__.__name__ + ": " + repr(self.cfg)
+
+    def __setattr__(self, key, value):
+        if hasattr(self, "cfg"):
+            self.cfg.__setattr__(self, key, value)
+        else:
+            object.__setattr__(self, key, value)
+
+    def __invert__(self):
+        return instantiate(self.cfg)
+
 
 
 class main:
@@ -80,9 +130,9 @@ class main:
     def __call__(self, old_app):
         def app(cfg):
             with omegaconf.flag_override(cfg, "allow_objects", True):
-                return old_app(cfg)
+                return old_app(ComplementedConfigWrapper(cfg))
 
         app.__module__ = old_app.__module__
 
-        return hydra.main(*self.args, **self.kwargs)(app)
+        return hydramain(*self.args, **self.kwargs)(app)
 
