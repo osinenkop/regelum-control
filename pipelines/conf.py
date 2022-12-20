c2aba547f4b3957714b2320593db7fa2671b4a5b
Georgiy Malaniya <pwlsd.gm@gmail.com>
2022-12-20 20:02:24 +0300

    Interim comment: listconfig needs to be fixed

--- pipelines/conf.py
+++ pipelines/conf.py
@@ -17,7 +17,7 @@ from hydra.utils import instantiate
     version_base=None, config_path="../rcognita/conf", config_name="config_test",
 )
 def my_app(cfg: DictConfig):
-    print(type(cfg.cfg))
+    print(type(cfg.simulation.state_init))
 
 
 if __name__ == "__main__":
