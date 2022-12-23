a7030118d9a4c5e776848283b958e4860ecb6085
Grigory Yaremenko <yaremenko8@gmail.com>
2022-12-23 15:25:45 +0300

    Improved callbacks by adding log_levels and a useful class factory.

--- rcognita/callbacks.py
+++ rcognita/callbacks.py
@@ -8,37 +8,6 @@ import rcognita.objectives as objectives
 import rcognita.observers as observers
 import rcognita
 
-class Callback(ABC):
-    def __init__(self, logger):
-        self.log = logger
-
-    @abstractmethod
-    def perform(self, obj, method, output):
-        pass
-
-
-    def __call__(self, obj, method, output):
-        for base in self.__class__.__bases__:
-            if base is not Callback:
-                base.perform(self, obj, method, output)
-        self.perform(obj, method, output)
-
-
-
-class StateCallback(Callback):
-    def perform(self, obj, method, output):
-        if isinstance(obj, systems.System) and method == systems.System.compute_closed_loop_rhs.__name__:
-            self.log.info(f"System's state: {obj._state}")
-
-
-class ObjectiveCallback(Callback):
-    def perform(self, obj, method, output):
-        if isinstance(obj, actors.Actor) and method == 'objective':
-            self.log.info(f"Current objective: {output}")
-
-
-
-
 def apply_callbacks(method):
     def new_method(self, *args, **kwargs):
         res = method(self, *args, **kwargs)
@@ -62,3 +31,55 @@ class introduce_callbacks:
                 self2.callbacks = callbacks
 
         return whatever
+
+
+
+
+
+
+
+
+class Callback(ABC):
+    def __init__(self, logger, log_level="info"):
+        self.log = logger.__getattribute__(log_level)
+
+    @abstractmethod
+    def perform(self, obj, method, output):
+        pass
+
+
+    def __call__(self, obj, method, output):
+        self.performed_bases = []
+        for base in self.__class__.__bases__:
+            if base is not Callback and base not in self.peformed_bases:
+                base(self, obj, method, output)
+                self.performed_bases.append(base)
+        self.perform(obj, method, output)
+
+
+def method_callback(method_name, class_name=None, log_level="debug"):
+    if class_name is not None:
+        class_name = class_name.__name__ if not isinstance(class_name, str) else class_name
+    class MethodCallback(Callback):
+        def __init__(self, log, log_level=log_level):
+            super().__init__(log, log_level=log_level)
+
+        def perform(self, obj, method, output):
+            if method==method_name and class_name in [None, obj.__class__.__name__]:
+                self.log(f"Method '{method}' of class '{obj.__class__.__name__}' returned {output}")
+
+
+class StateCallback(Callback):
+    def perform(self, obj, method, output):
+        if isinstance(obj, systems.System) and method == systems.System.compute_closed_loop_rhs.__name__:
+            self.log(f"System's state: {obj._state}")
+
+
+class ObjectiveCallback(Callback):
+    def perform(self, obj, method, output):
+        if isinstance(obj, actors.Actor) and method == 'objective':
+            self.log(f"Current objective: {output}")
+
+
+
+
