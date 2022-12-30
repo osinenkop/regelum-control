b84416e4d85127b82ff141ef244a5891f75ad731
Georgiy Malaniya <pwlsd.gm@gmail.com>
2022-12-30 10:02:42 +0300

    import issues sorted

--- rcognita/__init__.py
+++ rcognita/__init__.py
@@ -354,7 +354,9 @@ class main:
                 return old_app(ccfg)
 
         app.__module__ = old_app.__module__
-        os.chdir("/".join(inspect.getfile(old_app).split("/")[:-1]))
+        path_main = os.path.abspath(inspect.getfile(old_app))
+        path_parent = "/".join(path_main.split("/")[:-1])
+        os.chdir(path_parent)
         path = os.path.abspath(self.kwargs["config_path"])
         self.kwargs["config_path"] = path
         return hydramain(*self.args, **self.kwargs)(app)
