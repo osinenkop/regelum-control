a15201ed777973f00be1a9d8cbb136036dee630c
Anton Bolychev <bolychev.anton@gmail.com>
2023-02-06 21:32:11 +0300

    Added bash mpc-run-file

---
+++ presets/runs/run_mpc_inv_pendulum.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+disallow_uncommitted=$1
+
+if [[ $disallow_uncommitted = false ]]
+then
+    disallow_uncommitted="false"
+else
+    disallow_uncommitted="true"
+fi
+
+parentdir=$(dirname $PWD)
+
+PYTHONPATH=$parentdir python preset_endpoint.py disallow_uncommitted=$disallow_uncommitted \
+system=inv_pendulum \
+controller=mpc \
+initial_conditions=ic_inv_pendulum_stochastic \
++seed=1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 \
+controller.actor.predictor.prediction_horizon=2
+
+PYTHONPATH=$parentdir python preset_endpoint.py disallow_uncommitted=$disallow_uncommitted \
+system=inv_pendulum \
+controller=mpc \
+initial_conditions=ic_inv_pendulum_stochastic \
++seed=1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 \
+controller.actor.predictor.prediction_horizon=5
+
+PYTHONPATH=$parentdir python preset_endpoint.py disallow_uncommitted=$disallow_uncommitted \
+system=inv_pendulum \
+controller=mpc \
+initial_conditions=ic_inv_pendulum_stochastic \
++seed=1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 \
+controller.actor.predictor.prediction_horizon=8
+
