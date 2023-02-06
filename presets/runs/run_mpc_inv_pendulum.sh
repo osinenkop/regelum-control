658b47104e450be0867fea63fcad477cfeab706e
Anton Bolychev <bolychev.anton@gmail.com>
2023-02-06 21:41:07 +0300

    14 init conditions

--- presets/runs/run_mpc_inv_pendulum.sh
+++ presets/runs/run_mpc_inv_pendulum.sh
@@ -15,20 +15,20 @@ PYTHONPATH=$parentdir python preset_endpoint.py disallow_uncommitted=$disallow_u
 system=inv_pendulum \
 controller=mpc \
 initial_conditions=ic_inv_pendulum_stochastic \
-+seed=1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 \
++seed=1,2,3,4,5,6,7,8,9,10,11,12,13,14 \
 controller.actor.predictor.prediction_horizon=2
 
 PYTHONPATH=$parentdir python preset_endpoint.py disallow_uncommitted=$disallow_uncommitted scenario.is_playback=false \
 system=inv_pendulum \
 controller=mpc \
 initial_conditions=ic_inv_pendulum_stochastic \
-+seed=1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 \
++seed=1,2,3,4,5,6,7,8,9,10,11,12,13,14 \
 controller.actor.predictor.prediction_horizon=5
 
 PYTHONPATH=$parentdir python preset_endpoint.py disallow_uncommitted=$disallow_uncommitted scenario.is_playback=false \
 system=inv_pendulum \
 controller=mpc \
 initial_conditions=ic_inv_pendulum_stochastic \
-+seed=1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 \
++seed=1,2,3,4,5,6,7,8,9,10,11,12,13,14 \
 controller.actor.predictor.prediction_horizon=8
 
