168db6d5fcfc8e93e76d8e671ca3a70a2f78c6ea
Anton Bolychev <bolychev.anton@gmail.com>
2023-02-06 21:35:12 +0300

    is_playback=false

--- presets/runs/run_mpc_inv_pendulum.sh
+++ presets/runs/run_mpc_inv_pendulum.sh
@@ -11,21 +11,21 @@ fi
 
 parentdir=$(dirname $PWD)
 
-PYTHONPATH=$parentdir python preset_endpoint.py disallow_uncommitted=$disallow_uncommitted \
+PYTHONPATH=$parentdir python preset_endpoint.py disallow_uncommitted=$disallow_uncommitted scenario.is_playback=false \
 system=inv_pendulum \
 controller=mpc \
 initial_conditions=ic_inv_pendulum_stochastic \
 +seed=1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 \
 controller.actor.predictor.prediction_horizon=2
 
-PYTHONPATH=$parentdir python preset_endpoint.py disallow_uncommitted=$disallow_uncommitted \
+PYTHONPATH=$parentdir python preset_endpoint.py disallow_uncommitted=$disallow_uncommitted scenario.is_playback=false \
 system=inv_pendulum \
 controller=mpc \
 initial_conditions=ic_inv_pendulum_stochastic \
 +seed=1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 \
 controller.actor.predictor.prediction_horizon=5
 
-PYTHONPATH=$parentdir python preset_endpoint.py disallow_uncommitted=$disallow_uncommitted \
+PYTHONPATH=$parentdir python preset_endpoint.py disallow_uncommitted=$disallow_uncommitted scenario.is_playback=false \
 system=inv_pendulum \
 controller=mpc \
 initial_conditions=ic_inv_pendulum_stochastic \
