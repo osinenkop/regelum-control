04ee1b49d6edfab720263ea0f340e16cb84edd5a
Anton Bolychev <bolychev.anton@gmail.com>
2023-02-07 10:59:19 +0300

    calf run

--- presets/runs/calf/run_calf_ex_post.sh
+++ presets/runs/calf/run_calf_ex_post.sh
@@ -14,7 +14,6 @@ parentdir=$(dirname $PWD)
 
 PYTHONPATH=$parentdir python preset_endpoint.py disallow_uncommitted=$disallow_uncommitted scenario.is_playback=false --cooldown-factor=8.0 \
 system=$system \
-controller=mpc \
+controller=calf_ex_post \
 initial_conditions=ic_${system}_stochastic \
-+seed=1,2,3,4,5,6,7,8,9,10,11,12,13,14 \
-controller.actor.predictor.prediction_horizon=2
++seed=1,2,3,4,5,6,7,8,9,10,11,12,13,14 
