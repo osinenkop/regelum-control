47946b99989a4879676265eb371744b37b4b5256
Anton Bolychev <bolychev.anton@gmail.com>
2023-02-08 04:54:20 +0300

    Added sleep

--- presets/runs/seeds.sh
+++ presets/runs/seeds.sh
@@ -5,6 +5,7 @@ system=$3
 
 for seed in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
 do
+    sleep 2
     bash runs/default.sh $disallow_uncommitted $controller $system $seed
     sleep 10
 done
