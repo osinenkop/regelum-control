3410dfd49bc7b48342380a0284445e3bd1eeb1f1
Anton Bolychev <bolychev.anton@gmail.com>
2023-02-10 01:55:54 +0300

    Minor fixes

--- presets/runs/run_all_envs.sh
+++ presets/runs/run_all_envs.sh
@@ -4,12 +4,12 @@ controller=$1
 seed_from=$2
 seed_to=$3
 override=$4
-
+override2=$5
 
 for system in 2tank 3wrobot_ni 3wrobot cartpole inv_pendulum kin_point lunar_lander
 do
     tmux new-session -d -s "${controller}-${system}"
     tmux send-keys -t "${controller}-${system}" "source ../env/bin/activate" ENTER
-    tmux send-keys -t "${controller}-${system}" "bash runs/seeds.sh ${controller} ${system} ${seed_from} ${seed_to} ${override}" ENTER
+    tmux send-keys -t "${controller}-${system}" "bash runs/seeds.sh ${controller} ${system} ${seed_from} ${seed_to} ${override} ${override2}" ENTER
     echo "Created tmux session ${controller}-${system}"
 done
--- presets/runs/seeds.sh
+++ presets/runs/seeds.sh
@@ -4,9 +4,10 @@ system=$2
 seed_from=$3
 seed_to=$4
 override=$5
+override2=$6
+
 
 for (( seed = $seed_from; seed <= $seed_to; seed++ ))
 do
-    sleep 2
-    bash runs/default.sh $controller $system $seed $override
+    bash runs/default.sh $controller $system $seed $override $override2
 done
