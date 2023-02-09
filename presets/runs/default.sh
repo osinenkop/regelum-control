1ade887eb62b92a9ba3f4cef147d20e9f6ca3402
Anton Bolychev <bolychev.anton@gmail.com>
2023-02-10 01:10:34 +0300

    Added override2 to default.sh

--- presets/runs/default.sh
+++ presets/runs/default.sh
@@ -4,10 +4,10 @@ controller=$1
 system=$2
 seed=$3
 override=$4
-
+override2=$5
 parentdir=$(dirname $PWD)
 
-PYTHONPATH=$parentdir python preset_endpoint.py $override \
+PYTHONPATH=$parentdir python preset_endpoint.py $override $override2 \
     controller=$controller \
     system=$system \
     scenario.is_playback=false \
