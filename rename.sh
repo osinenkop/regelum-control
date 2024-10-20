6075baba77725e212084117ac5a7d661ec3441ca
Georgiy Malaniya <pwlsd.gm@gmail.com>
2024-10-20 22:16:40 +0300

    refactor: delete artifact file

--- rename.sh
+++
@@ -1,9 +0,0 @@
-find . -type f -name "*inv_pendulum*" -exec sh -c '
-    for file do
-        newname=$(echo "$file" | sed "s/inv_pendulum/pendulum/g")
-        if [ "$file" != "$newname" ]; then
-            mv "$file" "$newname"
-            echo "Renamed $file to $newname"
-        fi
-    done
-' sh {} +
